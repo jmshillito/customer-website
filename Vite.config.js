1d1ac2d227263636895c24891cba2355fe6e6000
jmshillito <jmshillito@yahoo.com>
2025-10-18 10:29:36 -0500

    Update Vite.config.js

--- Vite.config.js
+++ Vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    allowedHosts: ['preview--wonderfully-tremendous-target.mimo-machines.svc.cluster.local']
+    allowedHosts: ['*']
   }
 })
